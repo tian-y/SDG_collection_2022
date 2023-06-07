bebdd127d2e7ec4a346d1ebb312a3e915bbbd418
tian-y <y.tian.mailbox@gmail.com>
2023-06-07 17:34:43 +0200

    slight changes to standardise for reporting

--- Code/2023/11. merge to a single data file for reporting.R
+++ Code/2023/11. merge to a single data file for reporting.R
@@ -4,6 +4,10 @@ df_sdg_1718 <- readRDS(file = "Data/2023/10.2 17.18 correcting errors.RDS")
 df_sdg_1719 <- readRDS(file = "Data/2023/10. 17.19 filling missing values.RDS")
 df_sdg <- rbind(df_sdg_1718, df_sdg_1719)
 
+df_sdg <- df_sdg %>% 
+  mutate(total = as.character(total), 
+         total = ifelse(is.na(total), "NaN", total))
+
 df_sdg %>% 
   saveRDS("data/2023/10 all SDG ready to be reported in the simplest format.RDS")
 
@@ -12,10 +16,10 @@ df_sdg %>%
 #   mutate(country = countrycode(m49,"iso3n", "country.name"), 
 #          iso = countrycode(m49,"iso3n", "iso3c")) 
 
-df_sdg %>%   
-  filter(year == 2021) %>% 
-  filter(indicator != "17.19.1") %>% 
-  filter(total < 2) %>%
-  select(indicator, total) %>% 
-  table
+# df_sdg %>%   
+#   filter(year == 2021) %>% 
+#   filter(indicator != "17.19.1") %>% 
+#   filter(total < 2) %>%
+#   select(indicator, total) %>% 
+#   table
 
