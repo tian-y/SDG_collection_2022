10571e93c37b0b76f355a8085fe0353f5b520295
tian-y <y.tian.mailbox@gmail.com>
2023-06-12 16:55:58 +0200

    minor updates

--- Code/2023/11. merge to a single data file for reporting.R
+++ Code/2023/11. merge to a single data file for reporting.R
@@ -4,8 +4,7 @@ df_sdg_1718 <- readRDS(file = "Data/2023/10.2 17.18 correcting errors.RDS")
 df_sdg_1719 <- readRDS(file = "Data/2023/10. 17.19 filling missing values.RDS")
 df_sdg <- rbind(df_sdg_1718, df_sdg_1719)
 
-df_sdg %>% 
-  saveRDS("data/2023/10 all SDG ready to be reported in the simplest format.RDS")
+saveRDS(df_sdg,"data/2023/10 all SDG ready to be reported in the simplest format.RDS")
 
 
 # df_sdg <- df_sdg %>% 
