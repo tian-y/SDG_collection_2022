fe5539d6dcebc9763f192874e029c1305688536e
tian-y <y.tian.mailbox@gmail.com>
2025-02-25 12:14:07 +0100

    clean for sharing

--- Code/2023/main.R
+++ Code/2023/main.R
@@ -5,21 +5,23 @@
 # 
 # vec_files <- vec_files[order(vec_files)]
 rm(list = ls())
+# processing survey data
 source("code/2023/02. numerise data.R")
-source("code/2023/03. adding country names and codes.R")
-source("code/2023/04.1 use previous year's reporting.R")
-source("code/2023/04.3 ext using CTGAP 2022.R")
-source("code/2023/04.4 ext using NSDS status table.R")
-source("code/2023/05. ext the survey data.R")
-source("code/2023/06.1 fix multiple recipient issues in 17.19.1.R")
-source("code/2023/06.2 combine all fixed region results together.R")
+source("code/2023/03. adding country names and codes_2024.R.R")
+
+# solving issues in PRESS data for reporting
+source("code/2023/06.1 fix multiple recipient issues in 17.19.1_2024.R.R")
+source("code/2023/06.2 combine all fixed region results together_2024.R.R")
 source("code/2023/07. create data for indicator 17.19.1.R")
-source("code/2023/08. link old data with new data for 17.18.R")
+
+# final cleaning and merge data for pulication
+source("code/2023/08. link old data with new data for 17.18_2024.R.R")
 source("code/2023/09. create data for 17.18.R")
 
 source("code/2023/10.1 clean up and fill missing values with NA for all.R")
+source("code/2023/10.3 regroup 17.18 after correcting errors_2024.R.R")
 
-source("code/2023/10.2 correcting errors.R")
-source("code/2023/10.3 regroup 17.18 after correcting errors.R")
 source("code/2023/11. merge to a single data file for reporting.R")
+
+# prepare for reporting
 source("code/2023/12.1 prepare names for codes.R")
