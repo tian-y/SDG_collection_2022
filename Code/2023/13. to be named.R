d4677e8c52c413562df42bade5ecdb9082ba8584
tian-y <y.tian.mailbox@gmail.com>
2025-02-25 11:45:45 +0100

    minor change to improve workflow

--- Code/2023/13. to be named.R
+++ Code/2023/13. to be named.R
@@ -1,10 +1,12 @@
 rm(list = ls())
 gc()
 
-df_sdg <- readRDS("data/2023/08. full 17.18 data without regional aggregation.RDS")
+df_sdg <- readRDS(paste0("data/", 
+                         year(Sys.Date()),
+                         "/08. full 17.18 data without regional aggregation.RDS"))
 
 df_sdg %>% 
-  filter(year == 2022, indicator == "nsds_implement", region_code==202)
+  filter(year == 2023, indicator == "nsds_implement", region_code==202)
 
 df_sdg_region <- df_sdg %>% 
   filter(indicator %in% c("nsds_implement", "nsds_funded")) %>% 
